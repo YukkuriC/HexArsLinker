086346012feaab8fb0b2e2533df6760633b38561
YukkuriC <799433638@qq.com>
2025-03-21 20:12:07 +0800

    ambit for callback glyph

--- src/main/java/io/yukkuric/hex_ars_link/env/GlyphCallbackCastEnv.kt
+++ src/main/java/io/yukkuric/hex_ars_link/env/GlyphCallbackCastEnv.kt
@@ -6,7 +6,13 @@ import at.petrak.hexcasting.api.casting.eval.vm.CastingVM
 import at.petrak.hexcasting.api.casting.iota.Iota
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.InteractionHand
+import net.minecraft.world.phys.Vec3
 
-class GlyphCallbackCastEnv(caster: ServerPlayer) : StaffCastEnv(caster, InteractionHand.MAIN_HAND) {
+class GlyphCallbackCastEnv(caster: ServerPlayer, val hitPos: Vec3) : StaffCastEnv(caster, InteractionHand.MAIN_HAND) {
     fun getVM(init: Iota) = CastingVM(CastingImage().copy(stack = listOf(init)), this)
+    val GLYPH_RANGE = 8.0
+    val GLYPH_RANGE_SQ = Math.pow(GLYPH_RANGE, 2.0) + 1e-8
+
+    override fun isVecInRangeEnvironment(vec: Vec3) = if (vec.distanceToSqr(hitPos) <= GLYPH_RANGE_SQ) true
+    else super.isVecInRangeEnvironment(vec)
 }
--- src/main/java/io/yukkuric/hex_ars_link/glyph/HexCallbackSpellPart.java
+++ src/main/java/io/yukkuric/hex_ars_link/glyph/HexCallbackSpellPart.java
@@ -9,8 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
@@ -37,17 +36,19 @@ public class HexCallbackSpellPart extends AbstractEffect {
     @Override
     public void onResolveEntity(EntityHitResult rayTraceResult, Level world, @NotNull LivingEntity shooter, SpellStats spellStats, SpellContext spellContext, SpellResolver resolver) {
         if (!(shooter instanceof ServerPlayer player)) return;
-        this.onCastWithInitStack(new EntityIota(rayTraceResult.getEntity()), player);
+        var target = rayTraceResult.getEntity();
+        this.onCastWithInitStack(new EntityIota(target), player, target.position());
     }
     @Override
     public void onResolveBlock(BlockHitResult rayTraceResult, Level world, @NotNull LivingEntity shooter, SpellStats spellStats, SpellContext spellContext, SpellResolver resolver) {
         if (!(shooter instanceof ServerPlayer player)) return;
-        this.onCastWithInitStack(new Vec3Iota(rayTraceResult.getBlockPos().getCenter()), player);
+        var pos = rayTraceResult.getBlockPos().getCenter();
+        this.onCastWithInitStack(new Vec3Iota(pos), player, pos);
     }
-    public void onCastWithInitStack(Iota init, ServerPlayer player) {
+    public void onCastWithInitStack(Iota init, ServerPlayer player, Vec3 pos) {
         var spell = CallbackStorage.get(player);
         if (spell == null) return;
-        var env = new GlyphCallbackCastEnv(player);
+        var env = new GlyphCallbackCastEnv(player, pos);
         var vm = env.getVM(init);
         vm.queueExecuteAndWrapIotas(spell, env.getWorld());
     }
